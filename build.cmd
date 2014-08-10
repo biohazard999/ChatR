939efbbcc29111f3a21e4a86dc5b5cf84e599e18
Manuel Grundner <m.grundner@delegate.at>
2014-08-10 12:49:33 +0200

    added nuget restore

--- build.cmd
+++ build.cmd
@@ -2,4 +2,6 @@
 tools\nuget\nuget.exe update -Self
 tools\nuget\nuget.exe install Fake -OutputDirectory tools -ExcludeVersion
 
+tools\nuget\nuget.exe restore .
+
 tools\fake\tools\fake.exe build.fsx %*
